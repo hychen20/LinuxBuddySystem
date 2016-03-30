37263f4f68dc2e4cb74d667ca196cbf6100c6839
Huanyi Chen <huanyi-chen@foxmail.com>
2016-03-29 20:38:54 -0400

    Testing with random given order

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 #include "list.h"
 #include "helper.h"
 
+#include <cstdlib>
+#include "random"
+#include "math.h"
+#include <boost/date_time/posix_time/posix_time.hpp>
+
 #define MAX_ORDER 11
 
 using namespace std;
+using namespace boost::posix_time;
 
 extern struct page * mem_map_base;
 
@@ -104,19 +110,97 @@ void Buddy::free_pages(struct page *page, unsigned int order) {
 	// free_area[order].nr_free++;
 }
 
+// Avgs a double vector
+double avg(std::vector<double> d){
+  double sum = 0;
+
+  for(unsigned int i=0; i<d.size();i++){
+    sum += d[i];
+  }
+
+  if(d.size() > 0)
+    return sum/(float)d.size();
+  return 0;
+}
+
 int main() {
   mem_map_base = (struct page *)&mem_map;
-  Buddy buddy(mem_map);
-
+  std::vector<struct page *> alloc_page_p;
+  std::vector<int> alloc_page_order;
   struct page * page;
-  int i;
-  for (i=0; i<100; i++) {
-    page = buddy.alloc_pages(0);
-    cout << page << endl;
+  unsigned int i;
+
+  std::vector<double> initTime, free1Time, free2Time, allocate1Time, allocate2Time;
+  ptime initStart, initStop, allocate1Start, allocate1Stop, allocate2Start, allocate2Stop, free1Start, free1Stop, free2Start, free2Stop;
+
+  // Set random operations
+  std::random_device rd; // obtain a random number from hardware
+  std::mt19937 eng(rd()); // seed the generator
+  std::uniform_int_distribution<> distr(1, 10); // define the range
+
+  for(int i; i<100; i++) {
+    initStart = microsec_clock::universal_time();
+    Buddy buddy(mem_map);
+    initStop = microsec_clock::universal_time();
+
+    /* test 1 */
+    unsigned long psum = 0;
+    allocate1Start = microsec_clock::universal_time();
+    while(psum < 0.75*nPages){
+      int porder = distr(eng);
+      psum += 1 << porder;
+      page = buddy.alloc_pages(porder);
+      alloc_page_p.push_back(page);
+      alloc_page_order.push_back(porder);
+      // cout << page << ": " << porder << endl;
+    }
+    allocate1Stop = microsec_clock::universal_time();
+
+    free1Start =  microsec_clock::universal_time();
+    for (i=0; i<alloc_page_p.size() && psum > nPages/2; i++) {
+      int porder = alloc_page_order[i];
+      psum -= 1 << porder;
+      buddy.free_pages(alloc_page_p[i], porder);
+    }
+    free1Stop = microsec_clock::universal_time();
+
+    /* test 2 */
+    allocate2Start = microsec_clock::universal_time();
+    while(psum < 0.75*nPages){
+      int porder = distr(eng);
+      psum += 1 << porder;
+      page = buddy.alloc_pages(porder);
+      alloc_page_p.push_back(page);
+      alloc_page_order.push_back(porder);
+      // cout << page << ": " << porder << endl;
+    }
+    allocate2Stop = microsec_clock::universal_time();
+    free2Start = microsec_clock::universal_time();
+    for (i=0; i<alloc_page_p.size(); i++) {
+      buddy.free_pages(alloc_page_p[i], alloc_page_order[i]);
+    }
+    free2Stop = microsec_clock::universal_time();
+
+    boost::posix_time::time_duration initTimeD = -(initStart-initStop);
+    boost::posix_time::time_duration allocate1TimeD = -(allocate1Start - allocate1Stop);
+    boost::posix_time::time_duration allocate2TimeD = -(allocate2Start - allocate2Stop);
+    boost::posix_time::time_duration free1TimeD = -(free1Start - free1Stop);
+    boost::posix_time::time_duration free2TimeD = -(free2Start - free2Stop);
+
+    initTime.push_back(initTimeD.total_nanoseconds());
+    allocate1Time.push_back(allocate1TimeD.total_nanoseconds());
+    allocate2Time.push_back(allocate2TimeD.total_nanoseconds());
+    free1Time.push_back(free1TimeD.total_nanoseconds());
+    free2Time.push_back(free2TimeD.total_nanoseconds());
   }
-  for (i=0; i<100; i++) {
-    buddy.free_pages(page, 0);
-  }
+
+  std::cout<<"\nTime Taken";
+  std::cout<<"\n----------";
+  std::cout<<"\nInit time: "<< avg(initTime);
+  std::cout<<"\nAllocating 3/4th memory: "<<avg(allocate1Time);
+  std::cout<<"\nAllocating from 1/2 memory to 3/4th memory: "<<avg(allocate2Time);
+  std::cout<<"\nFreeing memory from 3/4 memory to 1/2: "<<avg(free1Time);
+  std::cout<<"\nFreeing memory from 3/4 memory to empty: "<<avg(free2Time)<<std::endl;
 
   return 0;
 }
